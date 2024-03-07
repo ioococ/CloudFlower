85d2d46227443494e51c8799e5bb14c726c01d2b
ioococ <cheerupi@outlook.com>
2024-03-07 16:59:49 +0800

    add necessary userservice 🧒

---
+++ userservice/src/main/java/ink/onei/user/UserApplication.java
@@ -0,0 +1,17 @@
+package ink.onei.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: nekotako
+ * @Description: UserService Main Class
+ * @Date: 07/03/2024 16:46 Thursday
+ */
+
+@SpringBootApplication
+public class UserApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserApplication.class);
+    }
+}
