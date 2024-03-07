62fa3ddfe79458eccc0522aeece1d6dbc3341a30
ioococ <cheerupi@outlook.com>
2024-03-07 16:44:54 +0800

    add powerful gateway ⛩️

---
+++ watchdog/src/main/java/ink/onei/watchdog/WatchdogApplication.java
@@ -0,0 +1,17 @@
+package ink.onei.watchdog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: nekotako
+ * @Description: Gateway Main Class
+ * @Date: 07/03/2024 16:19 Thursday
+ */
+
+@SpringBootApplication
+public class WatchdogApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WatchdogApplication.class);
+    }
+}
